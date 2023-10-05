1f43df311f918dccf4adba1adc5030bbe8bd9912
bob <bob@jadn.com>
2023-10-05 08:00:22 -0400

    lock down resource access

--- proxy-src/web_server.clj
+++ proxy-src/web_server.clj
@@ -11,21 +11,30 @@
 (def auth-param (str "&api_key=" GIANT_BOMB_KEY))
 
 (defn handle-api-request [request]
-  (let [ {:keys [uri query-string]} request  
+  (let [{:keys [uri query-string]} request
         resource-path (str uri "?" query-string)]
-        {:status 200
-         :body (:body (curl/get (str "https://www.giantbomb.com" resource-path auth-param)))}))
+    {:status 200
+     :body (:body (curl/get (str "https://www.giantbomb.com" resource-path auth-param)))}))
 
-(defn handle-local-request [ {:keys [uri]} ]
-  (let [ uri-cooked (if (= uri "/") "/index.html" uri) ]
+;; lock down resource snooping
+(def allowed-resources #{"/gbr.png",
+                         "/"
+                         "/js/main.js"})
+
+(defn handle-local-request [{:keys [uri]}]
+  (if (contains? allowed-resources uri)
+    (let [uri-cooked (if (= uri "/") "/index.html" uri)]
       {:status 200
        :headers {"Content-Type" (if (.endsWith uri-cooked ".png") "image/png" "text/html")}
-       :body (java.io.File. (str "public" uri-cooked))}))
+       :body (java.io.File. (str "public" uri-cooked))})
+    {:status 404
+     :headers {"Content-Type" "text/html"}
+     :body "Invalid resource."}))
 
 (defn handler [request]
-    (if (.startsWith (:uri request) "/api")
-      (handle-api-request request)
-      (handle-local-request request)))
+  (if (.startsWith (:uri request) "/api")
+    (handle-api-request request)
+    (handle-local-request request)))
 
 (defn start []
   (println "Listenting for web connections at :7878")
